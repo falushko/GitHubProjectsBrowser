55158ce146f850b562e4681633a962fd6bf0cef8
feriatos <vov8278@yandex.ru>
2015-07-28 16:33:30 +0300

    final version

--- config/db.php
+++ config/db.php
@@ -3,7 +3,7 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=gitSearch',
-    'username' => 'root',
-    'password' => '',
+    'username' => 'Vasya',
+    'password' => 'agH21jQJ',
     'charset' => 'utf8',
 ];
